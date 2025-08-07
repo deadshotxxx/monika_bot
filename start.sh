5bf2bbac15c8bcfb08f56c14e2279648010cca7d
deadshotxxx <elshadmuradov06@gmail.com>
2025-08-07 03:01:07 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
+
+# Создаём виртуальное окружение
+python3 -m venv venv
+
+# Активируем окружение
+source venv/bin/activate
+
+# Обновляем pip
+pip install --upgrade pip
+
+# Устанавливаем зависимости из requirements.txt
 pip install -r requirements.txt
+
+# Запускаем бота
 python3 main.py
